14f99b8f47aa72614dc9bde5c55817d47861ab7a
Caitlin Donahue <CaitlinDonahue95@gmail.com>
2025-03-16 14:59:45 -0500

    add mobile menu

--- mineral-collection-site/src/app/layout.tsx
+++ mineral-collection-site/src/app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from "next";
-import { Montserrat, Vollkorn } from "next/font/google";
-import "./globals.css";
-import { SanityLive } from "@/sanity/live";
-import styles from "./layout.module.css";
-import Link from "next/link";
+import type { Metadata } from 'next';
+import './globals.css';
+
+import { Montserrat, Vollkorn } from 'next/font/google';
+import Link from 'next/link';
+
+import { SanityLive } from '@/sanity/live';
+
+import NavMenu from './_shared/components/navMenu/navMenu';
+import styles from './layout.module.css';
 
 const montserrat = Montserrat({
-  variable: "--font-montserrat",
-  subsets: ["latin"],
+  variable: '--font-montserrat',
+  subsets: ['latin'],
 });
 
 const volkorn = Vollkorn({
-  variable: "--font-volkorn",
-  subsets: ["latin"],
+  variable: '--font-volkorn',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Mineral Collection",
+  title: 'Mineral Collection',
   description: "Caitlin's collection of rocks and minerals",
 };
 
@@ -26,19 +30,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${montserrat.style} ${volkorn.style}`}>
         <nav className={styles.nav}>
-          <Link href="/" className={styles.home}>
+          <Link href='/' className={styles.home}>
             CrystalDB
           </Link>
-          <Link href="/minerals">Minerals</Link>
-          <Link href="/rocks">Rocks</Link>
-          <Link href="/specimens">Specimens</Link>
+          <div className={styles.links}>
+            <Link href='/minerals'>Minerals</Link>
+            <Link href='/rocks'>Rocks</Link>
+            <Link href='/specimens'>Specimens</Link>
+          </div>
+          <div className={styles.mobileNav}>
+            <NavMenu />
+          </div>
         </nav>
-        <main className={styles.main}>
-          {children}
-        </main>
+        <main className={styles.main}>{children}</main>
         <SanityLive />
       </body>
     </html>
