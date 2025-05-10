7fceee8b5c7fadb09a092d7c8ba6cb67c861abb8
Caitlin Donahue <CaitlinDonahue95@gmail.com>
2025-05-09 21:28:19 -0500

    lint

--- mineral-collection-site/src/app/_shared/components/resultGrid/resultGrid.tsx
+++ mineral-collection-site/src/app/_shared/components/resultGrid/resultGrid.tsx
@@ -10,6 +10,7 @@ interface ResultGridProps {
 
 interface ResultGridGroupProps {
   title: string;
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
   items: any[];
   urlBase: string;
 }
